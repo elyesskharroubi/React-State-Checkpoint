badb4ae1d6408fb6b7e86a54e5d7a1131c748e3c
elyesskharroubi <medelyesskharroubi@outlook.com>
2020-11-30 00:37:06 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import "./components/Profile.css";
+// import pic from "./pexels-profile.jpg"
+import Profile from "./components/Profile"
 
 function App() {
+  const [profile, setProfile] = useState(true);
+  const toggle = () => setProfile(previous => !previous);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={profile ? "popUp" : ""}>
+      <Profile closing={toggle}/>
+    </div>
+      <button className="homeBtn" onClick={toggle}>SHOW PROFILE</button>
     </div>
   );
 }
---
+++ src/components/Profile.js
@@ -0,0 +1,19 @@
+import React from "react";
+import pic from "../pexels-profile.jpg";
+import "./Profile.css";
+
+export default function Profile(props) {
+  return (
+    <div className="profile">
+      <figure>
+        <img src={pic} alt="mypic" width="200" />
+      </figure>
+      <h1>John Doe</h1>
+      <h3>CEO of Nothing</h3>
+      <p>Erat nunc facilisis hendrerit enim elit congue.</p>
+      <button className="closeBtn" onClick={props.closing}>
+        Close
+      </button>
+    </div>
+  );
+}
